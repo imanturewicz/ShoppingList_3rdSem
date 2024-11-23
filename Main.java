15dea5b26b018e2cab88d4e22e32a51d91d043cc
imanturewicz <ignacy.manturewicz@gmail.com>
2024-11-23 21:30:13 +0100

    Reading and Writing to a file works very well :)

--- Main.java
+++ Main.java
@@ -7,55 +7,90 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<Product> products = new ArrayList<>();
-
-        System.out.print("How many items? ");
-        int howmany = scanner.nextInt();
-        
         String category, name, author;
         double price;
         char nutriscore;
 
-        for(int i = 1; i <= howmany; i++) {
-            System.out.print("\nPodaj "+i+" produkt:\nCategory: ");
-            category = scanner.next();
-            System.out.print("Name: ");
-            name = scanner.next();
-            System.out.print("Price: ");
-            price = scanner.nextDouble();
-            if(category.equals("Grocery")) {
-                System.out.print("Nutriscore: ");
-                nutriscore = scanner.next().charAt(0);
-                products.add(new Grocery(name, price, nutriscore));
-            }
-            else if(category.equals("Book")) {
-                System.out.print("Author: ");
-                author = scanner.next();
-                products.add(new Book(name, price, author));
+        System.out.print("Parse the shopping list from memory? [Y/n] ");
+        char parse = scanner.next().charAt(0);
+        String[] parts;
+
+        if(parse == 'Y') {
+            System.out.print("Provide the input file name (with extension): ");
+            String inputFilePath = scanner.next();
+
+            try(BufferedReader reader = new BufferedReader(new FileReader(inputFilePath))) {
+                String line;
+                while((line = reader.readLine()) != null) {
+                    // System.out.println("Reading line: " + line);
+                    if(line.endsWith(".")) {
+                        parts = line.split("\\s+");
+                        category = parts[1];
+                        name = parts[2];
+                        price = Double.parseDouble(parts[3].replace("$", ""));
+                        if (category.equals("Grocery")) {
+                            nutriscore = parts[5].charAt(0);
+                            products.add(new Grocery(name, price, nutriscore));
+                        } else if (category.equals("Book")) {
+                            author = parts[5].replace(".", "");
+                            products.add(new Book(name, price, author));
+                        }
+                    }
+                }
+            } catch(IOException e) {
+                System.err.println("Error reading the file: " + e.getMessage());
             }
         }
 
-        for (int i = 0; i < products.size(); i++) {
-            System.out.println();
-            products.get(i).show();
+        System.out.print("Manually add to the shopping list? [Y/n] ");
+        char manual = scanner.next().charAt(0);
+
+        if(manual == 'Y') {
+            System.out.print("How many items? ");
+            int howmany = scanner.nextInt();
+
+            for(int i = 1; i <= howmany; i++) {
+                System.out.print("\nPodaj "+i+" produkt:\nCategory (Grocery/Book): ");
+                category = scanner.next();
+                System.out.print("Name: ");
+                name = scanner.next();
+                System.out.print("Price: ");
+                price = scanner.nextDouble();
+                if(category.equals("Grocery")) {
+                    System.out.print("Nutriscore: ");
+                    nutriscore = scanner.next().charAt(0);
+                    products.add(new Grocery(name, price, nutriscore));
+                }
+                else if(category.equals("Book")) {
+                    System.out.print("Author: ");
+                    author = scanner.next();
+                    products.add(new Book(name, price, author));
+                }
+            }
         }
+
+        // for (int i = 0; i < products.size(); i++) {
+        //     System.out.println();                                    //wyświetlanie zawartości listy w konsoli
+        //     products.get(i).show();
+        // }
 
         Grocery grocery;
         Book book;
-        String filePath = "output.txt";
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+        String outputFilePath = "output.txt";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath))) {
             for(int i = 0; i < products.size(); i++) {
-                writer.write(i+1+":\n");
+                writer.write(i+1+": ");
                 if(products.get(i) instanceof Grocery) {
                     grocery = (Grocery) products.get(i);
-                    writer.write(grocery.getName()+" "
-                    +grocery.getPrice()+"$ nutriscore:"
-                    +grocery.getNutriscore());
+                    writer.write("Grocery "+grocery.getName()+" "
+                    +grocery.getPrice()+"$ nutriscore: "
+                    +grocery.getNutriscore()+".");
                 }
                 else if(products.get(i) instanceof Book) {
                     book = (Book) products.get(i);
-                    writer.write(book.getName()+" "
-                    +book.getPrice()+"$ Author:"
-                    +book.getAuthor());
+                    writer.write("Book "+book.getName()+" "
+                    +book.getPrice()+"$ Author: "
+                    +book.getAuthor()+".");
                 }
                 writer.newLine();
             }
