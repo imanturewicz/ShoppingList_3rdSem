e69442a74b62b4e4d120d6dcd417786f9f56c719
imanturewicz <ignacy.manturewicz@gmail.com>
2025-01-07 14:22:24 +0100

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,16 +1,16 @@
-public class Product {
+public class Product {        //superclass Product
     private String name = "XXX";
     private double price = -2137;
     private int quantity = 0;
 
-    public Product(String name, double price, int quantity) {
+    public Product(String name, double price, int quantity) {        //constructors
         this.name = name;
         this.price = price;
         this.quantity = quantity;
     }
     public Product() {}
 
-    public String getName() {
+    public String getName() {        //getters
         return name;
     }
     public double getPrice() {
@@ -20,11 +20,11 @@ public class Product {
         return quantity;
     }
 
-    public void setQuantity(int quantity) {
+    public void setQuantity(int quantity) {        ///setters
         this.quantity = quantity;
     }
 
-    public void show() {
+    public void show() {        //method outputting the attributes of the product
         System.out.println(getName());
         System.out.println(getPrice());
         System.out.println(getQuantity());
