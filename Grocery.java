5524fc09023f7589060e1c2854bd618416e03f51
imanturewicz <ignacy.manturewicz@gmail.com>
2025-01-07 14:28:26 +0100

    Update Grocery.java

--- Grocery.java
+++ Grocery.java
@@ -1,18 +1,18 @@
 public class Grocery extends Product {
     private char nutriScore = 'X';
 
-    public Grocery(String name, double price, int quantity, char nutriScore) {
+    public Grocery(String name, double price, int quantity, char nutriScore) {        //constructors
         super(name, price, quantity);
         this.nutriScore = nutriScore;
     }
     public Grocery() {}
 
-    public char getNutriScore() {
+    public char getNutriScore() {        //getter
         return nutriScore;
     }
 
     @Override
-    public void show() {
+    public void show() {        //method outputting the attributes of the product
         super.show();
         System.out.println(getNutriScore());
     }
